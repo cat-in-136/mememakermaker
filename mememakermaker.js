7688f40ec0c8d30ab284c99320a557d0cb074bbc
cat_in_136 <cat.in.136+github@gmail.com>
2014-06-05 01:20:17 +0900

    dynamic update and mamemaker2() now returns promise

--- mememakermaker.js
+++ mememakermaker.js
@@ -14,70 +14,96 @@
  */
 
 (function($) {
+  // TODO ES6 Promise migration (i.e. (new Promise(...) )
   $.fn.mememaker2 = function (options) {
     var defaults = {
       img: null
     };
     var setting = $.extend(defaults, options);
+
+    var that = this;
+    var defer = $.Deferred();
     var canvas = this[0];
+    var memeimg = null;
+
+    this.promise = defer.promise();
+
+    var updateSetting = this.updateSetting = function (options) {
+      setting = $.extend(setting, options);
+    };
+
+    var updateMeme = this.updateMeme = function () {
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(memeimg, 0, 0);
+      ctx.font        = "bold 20px sans-serif bold";
+      ctx.strokeStyle = "#000000";
+      ctx.fillStyle   = "#ffffff";
+  
+      for (var i = 1; setting["text" + i]; i++) {
+        var textsetting = setting["text" + i];
+        var textvalue = $(textsetting.value).val();
+        if (textvalue) {
+          ctx.textAlign = textsetting.textAlign || "center";
+          ctx.textBaseline = textsetting.textBaseline || "top";
+          var textX = textsetting.x;
+          if (!textX) {
+            if (ctx.textAlign == "left")   { textX = 0;                }
+            if (ctx.textAlign == "right")  { textX = canvas.width;     }
+            if (ctx.textAlign == "center") { textX = canvas.width / 2; }
+          }
+          var textY = textsetting.x;
+          if (!textY) {
+            if (ctx.textBaseline == "top")    { textY = 0;                }
+            if (ctx.textBaseline == "bottom") { textY = canvas.height - 20;}
+            if (ctx.textBaseline == "midlle") { textY = canvas.height / 2;}
+          }
+          if (textsetting.strokeStyle) { ctx.strokeStyle = textsetting.strokeStyle; }
+          if (textsetting.fillStyle)   { ctx.fillStyle   = textsetting.fillStyle;   }
+          ctx.strokeText(textvalue, textX, textY);
+          ctx.fillText(textvalue, textX, textY);
+        }
+      }
+    };
 
     (function delayImgLoad(url) {
-      // TODO ES6 Promise migration (i.e. (new Promise(...) )
-      var defer = $.Deferred();
+      var d = $.Deferred();
       try {
         var img = new Image();
-        img.onload = function () { defer.resolve(img); };
-        img.onerror = function (ex) { defer.reject(ex); };
+        img.onload = function () { d.resolve(img); };
+        img.onerror = function (ex) { d.reject(ex); };
         img.src = url;
       } catch (ex) {
-        defer.reject(ex);
+        d.reject(ex);
       }
-
-      return defer;
+      return d.promise();
     })(setting.img).then(function onFulFill (img) {
       try {
+        memeimg = img;
         canvas.width = img.width;
         canvas.height = img.height;
-  
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-        ctx.font        = "bold 20px sans-serif bold";
-        ctx.strokeStyle = "#000000";
-        ctx.fillStyle   = "#ffffff";
-  
-        for (var i = 1; setting["text" + i]; i++) {
-          var textsetting = setting["text" + i];
-          var textvalue = $(textsetting.value).val();
-          if (textvalue) {
-            ctx.textAlign = textsetting.textAlign || "center";
-            ctx.textBaseline = textsetting.textBaseline || "top";
-            var textX = textsetting.x;
-            if (!textX) {
-              if (ctx.textAlign == "left")   { textX = 0;                }
-              if (ctx.textAlign == "right")  { textX = canvas.width;     }
-              if (ctx.textAlign == "center") { textX = canvas.width / 2; }
-            }
-            var textY = textsetting.x;
-            if (!textY) {
-              if (ctx.textBaseline == "top")    { textY = 0;                }
-              if (ctx.textBaseline == "bottom") { textY = canvas.height - 20;}
-              if (ctx.textBaseline == "midlle") { textY = canvas.height / 2;}
-            }
-            if (textsetting.strokeStyle) { ctx.strokeStyle = textsetting.strokeStyle; }
-            if (textsetting.fillStyle)   { ctx.fillStyle   = textsetting.fillStyle;   }
-            ctx.strokeText(textvalue, textX, textY);
-            ctx.fillText(textvalue, textX, textY);
+
+        updateMeme();
+
+        if (!setting.noAutoUpdate) {
+          for (var i = 1; setting["text" + i]; i++) {
+            $(setting["text" + i].value).change(updateMeme);
+            $(setting["text" + i].value).keyup(updateMeme);
           }
         }
+
+        defer.resolve(that);
       } catch (ex) {
         console.error("Render error!", ex);
+        defer.reject(ex);
       }  
     }, function onReject(ex) {
       console.error("Image loading error!", ex);
+      defer.reject(ex);
     });
 
     return this;
   };
+
 })(jQuery);
 
 /* vim:set fileencoding=UTF-8 tabstop=2 shiftwidth=2 softtabstop=2 expandtab: */
